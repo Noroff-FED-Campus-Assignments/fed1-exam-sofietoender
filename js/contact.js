985645339487a7786f182181d9073eded83ad8e3
Sofie Tønder <105886175+sofietoender@users.noreply.github.com>
2023-05-19 13:08:17 +0200

    fixed form js

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,17 @@
 const formEl = document.querySelector("#js-form");
 const nameEl = document.querySelector("#fname");
 const lastNameEl = document.querySelector("#lname");
+const addressEl = document.querySelector("#address");
 const emailEl = document.querySelector("#email");
+const subjectEl = document.querySelector("#subject");
 const messageEl = document.querySelector("#message");
+
 
 const nameError = document.querySelector("#js-name-error");
 const emailError = document.querySelector("#js-email-error");
+const addressError = document.querySelector("#js-address-error");
 const messageError = document.querySelector("#js-message-error");
+const subjectError = document.querySelector("#js-subject-error");
 const lnameError = document.querySelector("#js-lname-error");
 
 formEl.addEventListener("submit", (event) => {
@@ -15,6 +20,8 @@ formEl.addEventListener("submit", (event) => {
   const name = nameEl.value;
   const lname = lastNameEl.value;
   const email = emailEl.value;
+  const address = addressEl.value;
+  const subject = subjectEl.value;
   const message = messageEl.value;
 
   if (name === "") {
@@ -32,6 +39,15 @@ formEl.addEventListener("submit", (event) => {
     return;
   }
 
+  if (address.length < 25) {
+    alert("Address must be more than 25 characters");
+    return;
+  }
+  if (subject === "") {
+    alert("Please enter a subject");
+    return;
+  }
+
   if (message === "") {
     alert("Please enter a message");
     return;
@@ -42,6 +58,8 @@ formEl.addEventListener("submit", (event) => {
   nameEl.value = "";
   lastNameEl.value = "";
   emailEl.value = "";
+  addressEl.value = "";
+  subjectEl.value = "";
   messageEl.value = "";
 });
 
@@ -88,7 +106,29 @@ emailEl.addEventListener("blur", (event) => {
     emailError.innerHTML = "";
   }
 });
+addressEl.addEventListener("blur", (event) => {
+  const address = event.target.value.trim();
 
+  const minLengthRegex = /^[a-zA-Z0-9_ ]{25,}/g;
+
+  if (!minLengthRegex.test(address)) {
+    addressError.innerHTML = "Address must be at least 25 characters long";
+  } else {
+    addressError.innerHTML = "";
+  }
+});
+
+subjectEl.addEventListener("blur", (event) => {
+  const subject = event.target.value.trim();
+
+  const minLengthRegex = /^[a-zA-Z0-9_ ]{10,}/g;
+
+  if (!minLengthRegex.test(subject)) {
+    subjectError.innerHTML = "Subject must be at least 10 characters long";
+  } else {
+    subjectError.innerHTML = "";
+  }
+});
 
 
 messageEl.addEventListener("blur", (event) => {
